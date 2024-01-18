4a57bbd5d09f38e42d434f76f9745135b536f500
Tomatofu <99457089+matofu@users.noreply.github.com>
2024-01-18 05:27:47 +0000

    Update main.py
    Switched to 'secrets' module instead of 'random' module

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import random
+import secrets
 import string
-chars = [list(string.punctuation), list(string.ascii_uppercase), list(string.ascii_lowercase), list(string.digits)] # Different characters that can be used
+chars = [string.punctuation, string.ascii_uppercase, string.ascii_lowercase, string.digits] # Different characters that can be used
 length = int(input('What length of password would you like?\n'))
 toggle_config = [int(input('Would you like special characters? (1/0)\n')), int(input('Would you like capital letters? (1/0)\n')), int(input('Would you like lowercase letters? (1/0)\n')), int(input('Would you like numbers? (1/0)\n'))]
 chars = list(filter(lambda x: toggle_config[chars.index(x)], chars)) # Filters out options from the list that the user doesn't want.
 try:
     password = ''
     for i in range(length):
-        password += random.choice(random.choice(chars))
-    print(f'Your password is {password}')
+        password += secrets.choice(secrets.choice(chars))
+    print(password)
 except:
     print('Something went wrong. You might want to choose different settings.')
