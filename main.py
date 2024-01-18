83dfcf021d15af240d4d66130d31abe0549130e8
Tomatofu <99457089+matofu@users.noreply.github.com>
2024-01-18 05:02:03 +0000

    Removed an unnecessary line

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import string
 chars = [list(string.punctuation), list(string.ascii_uppercase), list(string.ascii_lowercase), list(string.digits)] # Different characters that can be used
 length = int(input('What length of password would you like?\n'))
 toggle_config = [int(input('Would you like special characters? (1/0)\n')), int(input('Would you like capital letters? (1/0)\n')), int(input('Would you like lowercase letters? (1/0)\n')), int(input('Would you like numbers? (1/0)\n'))]
-chars_buffer = chars.copy() # Copies the list of characters to make sure the right indexes are being removed
-chars = list(filter(lambda x: toggle_config[chars_buffer.index(x)], chars)) # Filters out options from the list that the user doesn't want.
+chars = list(filter(lambda x: toggle_config[chars.index(x)], chars)) # Filters out options from the list that the user doesn't want.
 try:
     password = ''
     for i in range(length):
