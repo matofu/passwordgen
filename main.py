5220ba96b47db49af3aa8164fab1512516e1ac94
Tomatofu <99457089+matofu@users.noreply.github.com>
2024-01-18 16:19:42 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import secrets
 import string
 possible_chars = [string.punctuation, string.ascii_uppercase, string.ascii_lowercase, string.digits] # Different characters that can be used
 config = [input('Would you like to allow special characters? (1/0)\n'), input('Would you like to allow capital letters? (1/0)\n'), input('Would you like to allow lowercase letters? (1/0)\n'), input('Would you like to allow numbers? (1/0)\n'), input('What length would you like your password(s) to be? (integer)\n'), input('How may passwords would you like to generate? (integer)\n')]
-possible_chars = list(filter(lambda x: int(config[possible_chars.index(x)]), possible_chars)) # Filters out options from the list that the user doesn't want.
 try:
+    possible_chars = list(filter(lambda x: int(config[possible_chars.index(x)]), possible_chars)) # Filters out options from the list that the user doesn't want.
     for i in range(int(config[5])):
         password = ''
         for i in range(int(config[4])):
