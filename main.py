dcdd4fa6dc421bc9884ae76e5fbe690db2df7826
Tomatofu <99457089+matofu@users.noreply.github.com>
2024-03-04 00:31:18 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import secrets
 import string
 possible_chars = [string.punctuation, string.ascii_uppercase, string.ascii_lowercase, string.digits] # Different characters that can be used
-config = [input('Would you like to allow special characters? (1/0)\n'), input('Would you like to allow capital letters? (1/0)\n'), input('Would you like to allow lowercase letters? (1/0)\n'), input('Would you like to allow numbers? (1/0)\n'), input('What length would you like your password(s) to be? (integer)\n'), input('How may passwords would you like to generate? (integer)\n')]
+config = [input('What length would you like your password(s) to be? (integer)\n'), input('How may passwords would you like to generate? (integer)\n'), input('Would you like to allow special characters? (1/0)\n'), input('Would you like to allow capital letters? (1/0)\n'), input('Would you like to allow lowercase letters? (1/0)\n'), input('Would you like to allow numbers? (1/0)\n')]
 try:
-    possible_chars = list(filter(lambda x: int(config[possible_chars.index(x)]), possible_chars)) # Filters out options from the list that the user doesn't want.
-    for i in range(int(config[5])):
+    possible_chars = list(filter(lambda x: int(config[2:][possible_chars.index(x)]), possible_chars)) # Filters out options from the list that the user doesn't want.
+    for i in range(int(config[1])):
         password = ''
-        for i in range(int(config[4])):
+        for i in range(int(config[0])):
             password += secrets.choice(secrets.choice(possible_chars))
         print(f'\n{password}')
 except:
